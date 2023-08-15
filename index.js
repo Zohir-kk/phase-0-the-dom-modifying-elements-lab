fd677b048889d486bd6c10d309e7e6d7e1079ba0
zohir kioukiou <kiouzohir@gmail.com>
2023-08-15 23:41:35 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const MainRemove = document.querySelector('main#main')
+MainRemove.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.setAttribute('id','victory')
+
+newHeader.innerHTML="Zohir is the champion"
+document.body.append(newHeader)
